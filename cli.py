89c868b6907683705409b3742c6072593af2c0d2
Serhat BAL <serhat.bal@baogrenci.com>
2020-11-17 16:11:28 +0300

    birinci ve ikinci dosylar eklendi

---
+++ cli.py
@@ -0,0 +1,13 @@
+# cd => current directory
+# cd <Klasör adı> => sizi o dizine götürür. Örn cd Desktop
+# mkdir (windows/mac) klasör oluşturur. örn mkdir "Pyt4181"
+# md (Windows) klasör oluşturur. örnek "md Pyt4181"
+# cd .. bir üst dizine çıkar (geri)
+# (Windows) echo > cli.py       =>  bulunduğu dizin içerisinde cli.py adında bir belge oluşturur.
+# (Mac) touch cli.py            =>  bulunduğu dizin içerisinde cli.py adında bir belge oluşturur.
+
+# dir (Windows) ls (Mac) dizindeki dosyaları listeler.
+# rm belgeadı                   => adını verdiğiniz belgeyi siler.
+# rmdir dosyaadı                => adını verdiğiniz dosyayı siler.
+
+# tekli yorum satırı (#) bu alana yazılanlar kodlar derlenmez.
