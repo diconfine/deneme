49fa7c8083c7dd7bf7165a27a421d91825df8588
diconfine <serhatbal8@gmail.com>
2020-11-18 18:56:47 +0300

    ders icerik

---
+++ hata_yonetimi2.py
@@ -0,0 +1,14 @@
+try:
+    sayi1 = int(input("sayı 1 : "))
+    sayi2 = int(input("sayi 2 : "))
+    
+    toplam = sayi1 + sayi2
+    fark = sayi1 - sayi2
+    carpim = sayi1 * sayi2
+    bolum = sayi1 / sayi2
+    mod = sayi1 % sayi2
+
+    print(f"Sayıların Toplamı: {toplam}\n Sayıların Farkı : {fark}\n Sayıların Bölümü : {bolum} \n Sayıların Çarpımı : {carpim} \n Sayıların Modu : {mod}")
+except:
+    pass
+
